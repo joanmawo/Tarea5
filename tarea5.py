ee1857001ccc61a5645ec841237c66cdaa60f63d
Leonardo Rios <leo.re4y5@gmail.com>
2013-03-19 14:35:52 -0500

    0.2

--- tarea5.py
+++ tarea5.py
@@ -7,37 +7,46 @@ from scipy.linalg import eigh
 from scipy.optimize import curve_fit
 from scipy.fftpack import fft, fftfreq
 
-
 #datosFinales=open("data.dat", "w")
 
-data=np.loadtxt(open("monthrg.dat", "r"))
+data=np.loadtxt('monthrg.dat')
 
-print data
 
-NumeroManchas=data[:,3]
-Tiempo = data[:,0] + (1/12)*data[:,1] 
+
+Referencia=data[:,3]
+NumeroManchas=[]
+Tiempo = []
+PromedioManchas=[]
+AnioManchas=[]
+ManchasMensuales=[]
+
+for i in range(len(Referencia)):
+	temp=data[i,0]
+	if (temp>1795):
+		Tiempo.append(data[i,0] + (1.0/12.0)*data[i,1])
+		NumeroManchas.append(data[i,3])
 
 pylab.plot(Tiempo, NumeroManchas, '.')
-pylab.xlabel('Tiempo(años)')
+pylab.xlabel('Tiempo(anios)')
 pylab.ylabel('Numero de Manchas')
 pylab.title('Manchas vs Tiempo')
 pylab.show()
 
-
 fft_x = fft(NumeroManchas)/len(Tiempo) # FFT Normalized
 freq = fftfreq(len(Tiempo), 1/12) # Recuperamos las frecuencias
 pylab.plot(freq,np.abs(fft_x))
+
 print freq
 
-#Calcula el cuadrado de las potencias y prepara la gráfica de éstas versus la frecuencia
+#Calcula el cuadrado de las potencias y prepara la grafica de estas versus la frecuencia
+
 potencias = np.abs(fft_x)**2
 pylab.plot(freq, potencias)
 
-#Crea una tabla que sólo incluye las potencias de frecuecias entre 1 y 20 años
-tabla1[]
-for i 
+#Crea una tabla que solo incluye las potencias de frecuecias entre 1 y 20 anios
+tabla1=[]
+#for i
+#for frecuencia in freq
+#if (frecuencia > 1/20 and 1 > frecuencia)
+#tabla.append(1/frecuencia, )
 
-for frecuencia in freq
-	if (frecuencia > 1/20 and 1 > frecuencia)
-	tabla.append(1/frecuencia, )
-		
