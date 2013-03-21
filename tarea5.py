8a91e86098325c8765cc415ea8b5ea853db0d52d
Jorge Madrid <joanmawo@hotmail.com>
2013-03-21 00:42:12 -0500

    2.0

--- tarea5.py
+++ tarea5.py
@@ -33,9 +33,9 @@ pylab.title('Manchas vs Tiempo')
 pylab.savefig("Manchas")
 pylab.show()
 
-fft_x = fft(NumeroManchas)/len(Tiempo) # FFT Normalized
+fft_x = fft(NumeroManchas) #/len(Tiempo) # FFT Normalized
 freq = fftfreq(len(Tiempo), 1.0/12.0) # Recuperamos las frecuencias
-fft_x2 = fft(NumeroManchas)/len(Tiempo) #Para el punto 5to
+fft_x2 = fft(NumeroManchas) #/len(Tiempo) #Para el punto 5to
 pylab.plot(freq,np.abs(fft_x))
 
 
@@ -72,17 +72,51 @@ pylab.show()
 
 #Filtra las frecuencias
 for i in range(len(freq)):
-	if (freq[i]<=1.0/20.0): #Creo que la frecuencia debe ser menor a 1/20
+	if (freq[i]>=1.0/20.0): #Creo que la frecuencia debe ser menor a 1/20
 		fft_x2[i]=0.0 
+	if (freq[i]<=-1.0/20.0):
+		fft_x2[i]=0.0
 
-Inversa= ifft(fft_x2)
+Inv_limpia= ifft(fft_x2)
+Inv_limpia_real = np.real(Inv_limpia)
 
-#Aun no se que tengo que graficar
-pylab.plot(Inversa, Inversa, '.')
-pylab.xlabel('')
-pylab.ylabel('')
-pylab.title(' vs ')
-pylab.savefig("_")
+#Grafica el numero de manchas y la funcion ajustada contra el tiempo
+pylab.plot(Tiempo, NumeroManchas, '.')
+pylab.plot(Tiempo, Inv_limpia_real, linewidth=5, alpha = 0.5)
+pylab.xlabel('Tiempo (anios)')
+pylab.ylabel('Promedio de manchas solares')
+pylab.title('Promedio de manchas solares vs tiempo ')
+pylab.savefig("Ajuste")
 pylab.show()
+
+#Se ve que en la grafica de la potencia al cuadrado para periodos entre 1 y 20 anios el maximo esta en 11.1085 anios. Esto indica que el periodo del ciclo solar esta alrededor de este valor. La curva de ajuste del ciclo solar muestra un maximo en 1985. Los siguientes maximos solares ocurriran cada 11.1085 anios a partir de este.
+
+#Calculo del proximo maximo solar
+anio_actual = 2013
+anio = 1985
+periodo_solar = 11.1085
+encontrado = False
+
+maximos_solares_rango = []
+rango = 10
+i = 1
+
+while (encontrado == False):
+	anio = anio + periodo_solar	
+	if (anio >= anio_actual):
+		encontrado = True
+
+while(i < rango):
+	temp = anio + i*periodo_solar
+	maximos_solares_rango.append(temp)
+	i += 1
+
+print "El maximo solar siguiente sera en el "
+print anio
+
+print "Los 10 siguientes maximos solares ocurriran en "
+print maximos_solares_rango
+
+
 
 
